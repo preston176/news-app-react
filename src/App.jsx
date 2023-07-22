d527ca504067adc29bf465043db2ae15fcca1ffa
preston176 <prestonnyamweya@gmail.com>
2023-07-22 18:03:16 +0300

    grid tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [news, setNews] = useState([]);
+
+
+   const api = async () => {
+    let response = await fetch("https://newsapi.org/v2/everything?q=google&apiKey=2c903c8753634d1182e3043bdf6855ff")
+    let result = await response.json();
+    console.log(result);
+    setNews(result.articles)
+   }
+   useEffect(() => {
+    api()
+   },[])
+  
+console.log();
+
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+<>
+    <div className="grid gap-2 lg:grid-cols-4">
+    {news.map((items, key) => (
+        <div className="w-full rounded-lg shadow-md lg:max-w-sm" key={key}>
+            <img
+                className="object-cover w-full h-48"
+                src={items.urlToImage}
+                alt="image"
+            />
+            <div className="p-4">
+                <h4 className="text-xl font-semibold text-blue-600">
+                    
+                    {items.title}
+                </h4>
+                <p className="mb-2 leading-normal">
+                {items.content}
+                </p>
+                <button className="px-4 py-2 text-sm text-blue-100 bg-blue-500 rounded shadow">
+                    Read more
+                </button>
+            </div>
+        </div>
+    ))}
+</div>
+</>
   )
 }
 
