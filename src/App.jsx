d0adad0ed4efe3e12428b50fc242773dd01a23c1
preston176 <prestonnyamweya@gmail.com>
2023-07-22 18:08:37 +0300

    news grid component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react'
 import './App.css'
+import NewsGrid from './assets/Components/NewsGrid';
 
 function App() {
   const [news, setNews] = useState([]);
@@ -21,29 +22,7 @@ function App() {
   return (
 
 <>
-    <div className="grid gap-2 lg:grid-cols-4">
-    {news.map((items, key) => (
-        <div className="w-full rounded-lg shadow-md lg:max-w-sm" key={key}>
-            <img
-                className="object-cover w-full h-48"
-                src={items.urlToImage}
-                alt="image"
-            />
-            <div className="p-4">
-                <h4 className="text-xl font-semibold text-blue-600">
-                    
-                    {items.title}
-                </h4>
-                <p className="mb-2 leading-normal">
-                {items.content}
-                </p>
-                <button className="px-4 py-2 text-sm text-blue-100 bg-blue-500 rounded shadow">
-                    Read more
-                </button>
-            </div>
-        </div>
-    ))}
-</div>
+    <NewsGrid news={news} key="" />
 </>
   )
 }
---
+++ src/assets/Components/NewsGrid.jsx
@@ -0,0 +1,30 @@
+
+const NewsGrid = ({news, key}) => {
+  return (
+    <div className="grid gap-2 lg:grid-cols-4">
+    {news.map((items, key) => (
+        <div className="w-full rounded-lg shadow-md lg:max-w-sm" key={key}>
+            <img
+                className="object-cover w-full h-48"
+                src={items.urlToImage}
+                alt="image"
+            />
+            <div className="p-4">
+                <h4 className="text-xl font-semibold text-blue-600">
+                    
+                    {items.title}
+                </h4>
+                <p className="mb-2 leading-normal">
+                {items.content}
+                </p>
+                <button className="px-4 py-2 text-sm text-blue-100 bg-blue-500 rounded shadow">
+                    Read more
+                </button>
+            </div>
+        </div>
+    ))}
+</div>
+  )
+}
+
+export default NewsGrid
